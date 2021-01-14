cf50a8530eb60b3fb5ba06b55e4662f90e95f706
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-01-14 09:45:07 +0000

    Commited info

--- Exercise Files/Ch06/06_07/transcript.js
+++ Exercise Files/Ch06/06_07/transcript.js
@@ -24,3 +24,57 @@ function doubleIt(num) {
 
 // More info:
 // https://developer.mozilla.org/en-US/docs/JavaScript/Guide/Functions#Function_scope
+
+
+// Simple adding caculator
+
+
+
+
+function addingMachine() {
+  var total = 0;
+  
+  for (var i = 0; i < arguments.length; i += 1) {
+  var number = arguments[i];
+  
+
+  if (typeof number === "number") {
+  total += number;
+  }
+  }
+  return total; 
+  }
+  
+
+
+
+function addingMachine() {
+var total = 0;
+
+for (var i = 0; i < arguments.length; i += 1) {
+var number = arguments[i];
+
+if (typeof number === "number") {
+total += number;
+}
+}
+
+return total;
+}
+
+
+function addingMachine() {  // creates function
+  var total = 0; // initalise a verible 'total' with value of 0
+  
+  for (var i = 0; i < arguments.length; i += 1) { // arguments property creates an array and looks for length of array
+  var number = arguments[i]; 
+  
+  if (typeof number === "number") {
+  total += number;
+  }
+  }
+  
+  return total;
+  }
+  
+  addingMachine(1,2,3);
